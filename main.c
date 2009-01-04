bbc9a5696d6f70a476d97623f52eae65f021caf2
Sven Herzberg <sven@imendio.com>
2009-01-04 15:23:39 +0100

    another nice replacement

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ static AutoCompletion auto_completion[] = {
         {"c/o", "℅"},
         {"(c)", "©"},
         {"(R)", "®"},
-        // after newline: •
+        {"\n- ", "\n• "},
         {"->",  "→"},
         {":-)", "☺"}
 };
